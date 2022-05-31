4fa0b9ee52c3ae74e001acd84c07cba9091f572a
Gábor Farkas <gabor.farkas@gmail.com>
2022-05-31 13:25:28 +0200

    logs: move new-histogram next to old-histogram

--- public/app/features/explore/LogsContainer.tsx
+++ public/app/features/explore/LogsContainer.tsx
@@ -19,7 +19,7 @@ import { getTimeZone } from '../profile/state/selectors';
 import { LiveLogsWithTheme } from './LiveLogs';
 import { Logs } from './Logs';
 import { splitOpen } from './state/main';
-import { addResultsToCache, clearCache } from './state/query';
+import { addResultsToCache, clearCache, loadLogsVolumeData } from './state/query';
 import { updateTimeRange } from './state/time';
 import { LiveTailControls } from './useLiveTailControls';
 import { LogsCrossFadeTransition } from './utils/LogsCrossFadeTransition';
@@ -76,6 +76,7 @@ class LogsContainer extends PureComponent<LogsContainerProps> {
       logsMeta,
       logsSeries,
       logsQueries,
+      logsVolumeData,
       onClickFilterLabel,
       onClickFilterOutLabel,
       onStartScanning,
@@ -86,6 +87,7 @@ class LogsContainer extends PureComponent<LogsContainerProps> {
       scanning,
       range,
       width,
+      splitOpen,
       isLive,
       exploreId,
       addResultsToCache,
@@ -131,10 +133,13 @@ class LogsContainer extends PureComponent<LogsContainerProps> {
               logRows={logRows}
               logsMeta={logsMeta}
               logsSeries={logsSeries}
+              logsVolumeData={logsVolumeData}
               logsQueries={logsQueries}
               width={width}
+              splitOpen={splitOpen}
               loading={loading}
               loadingState={loadingState}
+              loadLogsVolumeData={loadLogsVolumeData}
               onChangeTime={this.onChangeTime}
               onClickFilterLabel={onClickFilterLabel}
               onClickFilterOutLabel={onClickFilterOutLabel}
@@ -200,6 +205,7 @@ const mapDispatchToProps = {
   splitOpen,
   addResultsToCache,
   clearCache,
+  loadLogsVolumeData,
 };
 
 const connector = connect(mapStateToProps, mapDispatchToProps);
